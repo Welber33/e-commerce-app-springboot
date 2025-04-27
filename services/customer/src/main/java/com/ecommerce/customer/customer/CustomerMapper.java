7bd78923a87812bc1151bb898481603eb2d647c5
Welber33 <jwwork21@gmail.com>
2025-04-27 18:40:24 +0100

    Implementation of Customer Microservice

---
+++ services/customer/src/main/java/com/ecommerce/customer/customer/CustomerMapper.java
@@ -0,0 +1,29 @@
+package com.ecommerce.customer.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerMapper {
+
+    public Customer toCustomer(CustomerRequest request) {
+        if (request == null) return null;
+
+        return Customer.builder()
+                .id(request.id())
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .address(request.address())
+                .email(request.email())
+                .build();
+    }
+
+    public CustomerResponse fromCustomer(Customer customer) {
+        return new CustomerResponse(
+                customer.getId(),
+                customer.getFirstname(),
+                customer.getLastname(),
+                customer.getEmail(),
+                customer.getAddress()
+        );
+    }
+}
